08a8afeec4cfe68157f754d2b2fc7660f484934f
Gujgiczer Máté <gujgi@wearebreak.com>
2015-04-08 21:04:07 +0200

    - admin site added

--- konferencia/routes/admin.js
+++ konferencia/routes/admin.js
@@ -2,26 +2,32 @@ var express = require('express');
 var sqlite = require('sqlite3');
 var router = express.Router();
 
-var contacts = new sqlite.Database('../contacts.db');
 var emails = new sqlite.Database('../emails.db');
+var date = new Date();
+var sqlname = "emails_" + (date.getYear() + 1900) + "_" + ((date.getMonth() + 1) < 7 ? "1" : "2");
 
 /* GET listings. */
 router.get('/', function(req, res) {
-  console.log(req.body);
+    console.log(req.body);
 
-    var contactResults = [];
-    var emailResults = [];
+    var emailHTMLCode = "<link href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css' rel='stylesheet'>" +
+        "<table style='width: 100%%; margin: 0; padding: 0;' class='table table-striped'>" +
+        "<tr> <th>ID</th> <th>Name</th> <th>Email</th> <th>From</th> <th>Info</th> </tr>"
 
-    contacts.each('SELECT * FROM contacts', function(err, row) {
-      contactResults.push(row);
+    emails.each('SELECT DISTINCT * FROM ' + sqlname + " group by EMAIL", function(err, row) {
+        emailHTMLCode +=
+        "<td>" + row.id + "</td>" +
+        "<td>" + row.name + "</td>" +
+        "<td>" + row.email + "</td>" +
+        "<td>" + row.comesfrom + "</td>" +
+        "<td>" + row.info + "</td>" +
+        "</tr>"
+    }, function(err, rows) {
+        emailHTMLCode += "</table>"
+        res.send(emailHTMLCode);
     });
 
 
-    emails.each('SELECT * FROM emails', function(err, row) {
-      emailResults.push(row);
-    });
-
-    res.send(JSON.stringify(emailResults) + JSON.stringify(contactResults));
 });
 
 module.exports = router;
