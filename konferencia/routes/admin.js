f8bf1c88c1fab59e418f2ed609eea4d3e3e1f1a5
Gujgiczer Máté <gujgi@wearebreak.com>
2015-04-18 18:50:26 +0200

    - admin modified

--- konferencia/routes/admin.js
+++ konferencia/routes/admin.js
@@ -8,26 +8,15 @@ var sqlname = "emails_" + (date.getYear() + 1900) + "_" + ((date.getMonth() + 1)
 
 /* GET listings. */
 router.get('/', function(req, res) {
-    console.log(req.body);
+    var count = 0;
+    var emailData = [];
 
-    var emailHTMLCode = "<link href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css' rel='stylesheet'>" +
-        "<table style='width: 100%%; margin: 0; padding: 0;' class='table table-striped'>" +
-        "<tr> <th>ID</th> <th>Name</th> <th>Email</th> <th>From</th> <th>Info</th> </tr>"
-
-    emails.each('SELECT DISTINCT * FROM ' + sqlname + " group by EMAIL", function(err, row) {
-        emailHTMLCode +=
-        "<td>" + row.id + "</td>" +
-        "<td>" + row.name + "</td>" +
-        "<td>" + row.email + "</td>" +
-        "<td>" + row.comesfrom + "</td>" +
-        "<td>" + row.info + "</td>" +
-        "</tr>"
+    emails.each('SELECT DISTINCT * FROM ' + sqlname + " GROUP BY EMAIL ORDER BY ID ASC", function(err, row) {
+        emailData.push({id : row.id, name : row.name, email : row.email, comesfrom : row.comesfrom, info : row.info});
+        count ++;
     }, function(err, rows) {
-        emailHTMLCode += "</table>"
-        res.send(emailHTMLCode);
+        res.render("admin",{titleCount: count, emails : emailData});
     });
-
-
 });
 
 module.exports = router;
