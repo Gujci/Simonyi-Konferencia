e3a139bb52c3db3ff02a01a83d4da68849bb6fff
Gujgiczer Máté <gujgi@wearebreak.com>
2015-04-22 14:13:59 +0200

    - feedback site modified - admin site upgrade

--- konferencia/routes/admin.js
+++ konferencia/routes/admin.js
@@ -3,19 +3,27 @@ var sqlite = require('sqlite3');
 var router = express.Router();
 
 var emails = new sqlite.Database('../emails.db');
+var feedbacks = new sqlite.Database('../feedback.db');
+
 var date = new Date();
-var sqlname = "emails_" + (date.getYear() + 1900) + "_" + ((date.getMonth() + 1) < 7 ? "1" : "2");
+var EMAILS_TABLE = "emails_" + (date.getYear() + 1900) + "_" + ((date.getMonth() + 1) < 7 ? "1" : "2");
+var FEEDBACK_TABLE = "feedback12";
 
 /* GET listings. */
 router.get('/', function(req, res) {
     var count = 0;
     var emailData = [];
+    var feedbackData = [];
 
-    emails.each('SELECT DISTINCT * FROM ' + sqlname + " GROUP BY EMAIL ORDER BY ID ASC", function(err, row) {
+    emails.each('SELECT DISTINCT * FROM ' + EMAILS_TABLE + " GROUP BY EMAIL ORDER BY ID ASC", function(err, row) {
         emailData.push({id : row.id, name : row.name, email : row.email, comesfrom : row.comesfrom, info : row.info});
         count ++;
     }, function(err, rows) {
-        res.render("admin",{titleCount: count, emails : emailData});
+        feedbacks.each('SELECT * FROM ' + FEEDBACK_TABLE, function(err, row){
+            feedbackData.push({name: row.name, email: row.email, text: row.feedback});
+        },function(err, rows) {
+            res.render("admin",{titleCount: count, emails : emailData, feedbacks: feedbackData});
+        });
     });
 });
 
